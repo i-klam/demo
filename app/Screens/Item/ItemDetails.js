5fb986315151b22f2eb83c5c427816274e92a1a9
klam <kklam431@gmail.com>
2025-02-08 18:45:16 +0100

    32r

--- app/Screens/Item/ItemDetails.js
+++ app/Screens/Item/ItemDetails.js
@@ -317,7 +317,7 @@ const ItemDetails = ({ navigation }) => {
                         </View>
                         <View style={{ flex: 1 }}>
                             <Button
-                                title="Make Offer "
+                                title="Make Offer"
                             />
                         </View>
                     </View>
